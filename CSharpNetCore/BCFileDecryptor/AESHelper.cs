217a416df376ea369d5519ac2599729c63b14482
Hussain Sandhu <hs@secomba.com>
2020-03-27 16:53:41 +0100

    Merge pull request #1 in ~HS/boxcryptor-single-file-decryptor from csharp-port to master
    Squashed commit of the following:
    
    commit 78a3e35ed33b5be513ada1325bb06ad2b1f5b473
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 25 10:28:03 2020 +0100
    
        Fixed bug that an exception was thrown when decrypting a file with no user generated contents
    
    commit db70fb2c1f7958e2e767f3b2c7ad07a22028a17f
    Author: hs <hs@secomba.com>
    Date:   Tue Mar 24 16:47:29 2020 +0100
    
        Resolved issues and added Readme.md with instructions for C#
    
    commit 42aa519a98d75870b959744d22982f765c698170
    Author: hs <hs@secomba.com>
    Date:   Mon Mar 23 09:19:27 2020 +0100
    
        Resolved some more issues
    
    commit 9076dadca0a2a623eb179f4821e4e09b629a30ef
    Author: hs <hs@secomba.com>
    Date:   Fri Mar 20 16:19:57 2020 +0100
    
        Resolved issues
    
    commit 63cfe36fdd8ac558b30301029af8f7e1ba61ddc1
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 14:29:00 2020 +0100
    
        removed bin / build files
    
    commit 8243bbe1ff6e240975b0510f366414b3f392b488
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 14:25:13 2020 +0100
    
        updated gitignore based on gitignore.io
    
    commit 8923e0fa50b04c08ca193c5339e954531b387f71
    Merge: 04371c3 c7f8b65
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 14:16:03 2020 +0100
    
        Merge branch 'master' into csharp-port
    
    commit 04371c37efee574443393c1718912b35c5950830
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:56:28 2020 +0100
    
        update gitignore
    
        (cherry picked from commit c7f8b65a40ad6f5f1343a7f45a9393c97de17c27)
    
    commit c132fa406a78745958860a01c3bb3e43c3ebfc68
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:50:30 2020 +0100
    
        updated gitignore
    
        (cherry picked from commit 5f92cf6802c01d5d14bd3e1be9412142696104e3)
    
    commit 953b90c350eecdb0c50301a7a7daa4070d16154d
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:37:44 2020 +0100
    
        initial commit for CSharpNetCore version
    
        (cherry picked from commit 97bab13855248a8f1f5cdf22895fcc112f976057)
    
    commit c7f8b65a40ad6f5f1343a7f45a9393c97de17c27
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:56:28 2020 +0100
    
        update gitignore
    
    commit 5f92cf6802c01d5d14bd3e1be9412142696104e3
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:50:30 2020 +0100
    
        updated gitignore
    
    commit 1ab756a4a5ec392f81ecd767e591118567383e50
    Merge: 97bab13 3389fd8
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:45:44 2020 +0100
    
        Merge branch 'master' of https://github.com/hs-sec/boxcryptor-single-file-decryptor
        initial merge
    
    commit 97bab13855248a8f1f5cdf22895fcc112f976057
    Author: hs <hs@secomba.com>
    Date:   Wed Mar 18 12:37:44 2020 +0100
    
        initial commit for CSharpNetCore version

---
+++ CSharpNetCore/BCFileDecryptor/AESHelper.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using static System.Buffer;
+
+namespace BCFileDecryptorCore
+{   
+    public class UnsupportedEncodingException : Exception
+    {
+        // in .NET Framework application this exception inherits 
+        // from System.IdentityModel.RequestException, which is 
+        // unavailable in .NET Core
+    }
+
+    class AESHelper
+    {
+        private static byte[] ComputeBlockIVec(byte[] IVec, long seed, byte[] key)
+        {
+            if (IVec.Length <= 0 || key.Length <= 0)
+            {
+                throw new Exception("Base initialization vector and crypto key can't be empty");
+            }
+
+            byte[] md = new byte[8];
+            for (int i = 0; i < 8; ++i)
+            {
+                md[i] = (byte)(seed & 0xff);
+                seed >>= 8;
+            }
+
+            byte[] data = new byte[IVec.Length + md.Length];
+            BlockCopy(IVec, 0, data, 0, IVec.Length);
+            BlockCopy(md, 0, data, IVec.Length, md.Length);
+
+            byte[] buffer = HashHelper.ComputeSHA256HMAC(data, key, true);
+            byte[] result = new byte[IVec.Length];
+            Array.Copy(buffer, 0, result, 0, IVec.Length);
+
+            return result;
+        }
+
+        public static byte[] DecryptData(byte[] data, byte[] cryptoKey, byte[] IVec, bool isUserGeneratedData)
+        {
+            return DecryptData(data, cryptoKey, IVec, isUserGeneratedData, PaddingMode.PKCS7);
+        }
+        public static byte[] DecryptData(byte[] data, byte[] cryptoKey, byte[] IVec, bool isUserGeneratedData, PaddingMode padding)
+        {
+            if ((isUserGeneratedData ? data.Length < 0 : data.Length <= 0) || cryptoKey.Length <= 0 || IVec.Length <= 0)
+            {
+                throw new Exception("Encrypted data, crypto key and initialization vector can't be empty");
+            }
+
+            byte[] result;
+            try
+            {
+                // PKCS7 padding (https://en.wikipedia.org/wiki/PKCS) is used in case the
+                // last data block is smaller than the block size used by AES (16 bytes)
+                using Aes aes = Aes.Create();
+                aes.Mode = CipherMode.CBC;
+                aes.Key = cryptoKey;
+                aes.IV = IVec;
+                aes.Padding = padding;
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+                using MemoryStream mStream = new MemoryStream();
+                using CryptoStream cStream = new CryptoStream(mStream, decryptor, CryptoStreamMode.Write);
+                cStream.Write(data, 0, data.Length);
+                cStream.FlushFinalBlock();
+                result = mStream.ToArray();
+
+            }
+            catch (ArgumentException e) 
+            {
+                throw new Exception("Data could not be decrypted", e);
+            }
+
+            return result;
+        }
+
+        public static byte[] DecryptDataPBKDF2(
+            string data,
+            string pbkdf2Password,
+            string pbkdf2Salt,
+            int pbkdf2Iterations)
+        {
+            if (pbkdf2Password.Equals("") || pbkdf2Salt.Equals("") || pbkdf2Iterations <= 0)
+            {
+                throw new Exception("Password and salt for the PBKDF2 algorithm can not be empty and the iteration count must be bigger than zero");
+            }
+
+            // data and salt are base 64 encoded
+            byte[] decodedPrivateKeyBytes = Base64Helper.decode(data);
+            byte[] decodedSalt = Base64Helper.decode(pbkdf2Salt);
+
+            // derive bytestream from password and salt
+            // via PBKDF2 - the resulting bytes (64) are
+            // two AES256 keys which will be used in further steps
+            PBKDF2Helper pbkdf2 = new PBKDF2Helper(pbkdf2Password, decodedSalt, pbkdf2Iterations);
+            byte[] hashBytes = pbkdf2.GetBytes(64);
+            int[] unsignedHashBytes = new int[64];
+            for (int i = 0; i < hashBytes.Length; ++i)
+            {
+                unsignedHashBytes[i] = hashBytes[i] & 0xFF;
+            }
+
+            byte[] cryptoKey = new byte[32];
+            BlockCopy(hashBytes, 0, cryptoKey, 0, 32);
+            byte[] hmacKey = new byte[hashBytes.Length - 32];
+            BlockCopy(hashBytes, 32, hmacKey, 0, 32);
+
+            // the encrypted data holds an initialization vector
+            // for the AES decryption, a HMAC-SHA-256 hash to
+            // verify the given input and the actual private key bytes
+            byte[] IVec = new byte[16];
+            BlockCopy(decodedPrivateKeyBytes, 0, IVec, 0, 16);
+            byte[] givenHmacHash = new byte[32];
+            BlockCopy(decodedPrivateKeyBytes, 16, givenHmacHash, 0, 32);
+            byte[] privateKeyBytes = new byte[decodedPrivateKeyBytes.Length - 48];
+            BlockCopy(decodedPrivateKeyBytes, 48, privateKeyBytes, 0, decodedPrivateKeyBytes.Length - 48);
+
+            // it is necessary to compute the HMAC-SHA-256 hash
+            // again to make sure the private key, password, salt
+            // and iteraton count weren't tampered with
+            byte[] computedHmacHash = HashHelper.ComputeSHA256HMAC(privateKeyBytes, hmacKey);
+            if (!computedHmacHash.SequenceEqual<byte>(givenHmacHash))
+            {
+                throw new Exception("HMAC hashes do not match, make sure you used a matching .bckey file and password");
+            }
+
+            byte[] result = DecryptData(privateKeyBytes, cryptoKey, IVec, false);
+
+            Console.WriteLine("AES decryption finished");
+            return result;
+        }
+
+        public static byte[] DecryptFile(
+            string encryptedFilePath,
+            byte[] fileCryptoKey,
+            string baseIVec,
+            int blockSize,
+            int offset,
+            int padding)
+        {
+            Console.WriteLine($"AES Decryption of file '{encryptedFilePath}' started");
+            if (fileCryptoKey.Length <= 0 || blockSize <= 0)
+            {
+                throw new Exception("Crypto key for file can't be empty and block size must be bigger than zero");
+            }
+
+            // read the encrypted file
+            byte[] fileBytes;
+            try
+            {
+                fileBytes = File.ReadAllBytes(Path.GetFullPath(encryptedFilePath));
+            }
+            catch (IOException e)
+            {
+                throw new Exception("Could not read file", e);
+            }
+
+            // IVec in file header is base 64 encoded
+            byte[] decodedFileIV = Base64Helper.decode(baseIVec);
+
+            // report initial status
+            int fileSize = fileBytes.Length;
+            long fileSizeFivePer = Convert.ToInt64(Math.Floor(fileSize * 0.05));    // 5% of file size; for status reporting
+            string byteProgress = $" (0 / {fileSize} bytes)";
+            StringBuilder routeString = new StringBuilder();
+            StringBuilder spaceString = new StringBuilder();
+            for (int i = 0; i < 20; i++)
+            {
+                spaceString.Append(" ");
+            }
+            Console.WriteLine($"Progress: [{spaceString}]{byteProgress}");
+
+            // decrypt each block separately with its own initialization vector
+            int blockNo = 0;
+            byte[] result = new byte[fileSize - offset - padding];
+            for (int byteNo = offset, nextStatusThreshold = offset, currentStep = 0; byteNo <= fileSize; byteNo += blockSize, ++blockNo)
+            {
+                byte[] blockIVec = AESHelper.ComputeBlockIVec(decodedFileIV, blockNo, fileCryptoKey);
+
+                // get the input data for the current block (the last block may be shorter than [blockSize] bytes)
+                int end = (byteNo + blockSize >= fileSize) ? fileSize : byteNo + blockSize;
+                byte[] blockInput = new byte[end - byteNo];
+                BlockCopy(fileBytes, byteNo, blockInput, 0, end - byteNo);
+
+                PaddingMode currentPadding = (end == fileSize && padding > 0) ? PaddingMode.PKCS7 : PaddingMode.None;
+
+                // get the decrypted data for this block ...
+                byte[] decryptedBlock = AESHelper.DecryptData(blockInput, fileCryptoKey, blockIVec, true, currentPadding);
+
+                // ... and append it to the previous data
+                BlockCopy(decryptedBlock, 0, result, byteNo - offset, decryptedBlock.Length);
+
+                // report intermediate status every 5% 
+                if (byteNo > nextStatusThreshold)
+                {
+                    int steps = byteNo / nextStatusThreshold;
+                    nextStatusThreshold += Convert.ToInt32(fileSizeFivePer * steps);
+
+                    currentStep += steps;
+                    byteProgress = $" ({byteNo} / {fileSize} bytes)";
+                    routeString.Length = 0;
+                    for (int i = 0; i < currentStep; i++) 
+                    { 
+                        routeString.Append("#"); 
+                    }
+                    spaceString.Length = 0;
+                    for (int i = 0; i < 20 - currentStep; i++)
+                    {
+                        spaceString.Append(" ");
+                    }
+                    Console.WriteLine($"Progress: [{routeString}{spaceString}]{byteProgress}");
+                }
+            }
+
+            // newline after Status report
+            byteProgress = $" ({fileSize} / {fileSize} bytes)";
+            routeString.Length = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                routeString.Append("#");
+            }
+            Console.WriteLine($"Progress: [{routeString}]{byteProgress}");
+
+            Console.WriteLine("AES decryption of file finished");
+            return result;
+        }
+    }
+}
+
---
+++ CSharpNetCore/BCFileDecryptor/RSAHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace BCFileDecryptorCore
+{
+    class RSAHelper
+    {
+        public static byte[] DecryptData(string encryptedFileKey, byte[] decryptedPrivateKey)
+        {
+            Console.WriteLine("RSA decryption of data has started");
+
+            if (decryptedPrivateKey.Length <= 0)
+            {
+                throw new Exception("The private key used for the RSA decryption can't be empty");
+            }
+
+            // encrypted file key is base 64 encoded
+            byte[] decodedFileKey = Base64Helper.decode(encryptedFileKey);
+
+            // private key is stored in a simplified PEM format (no header / footer) and no line breaks)
+            // decode it from base 64 again to get the DER encoding needed for the key spec
+            byte[] privateKeyDEREncoded = Base64Helper.decode(decryptedPrivateKey);
+
+            byte[] result;
+            try
+            {
+                // create / load a PrivateKey from the DER encoded key
+                RSA rsa = RSA.Create();
+                rsa.ImportRSAPrivateKey(privateKeyDEREncoded, out int bytesRead);
+                result = rsa.Decrypt(decodedFileKey, RSAEncryptionPadding.OaepSHA1);
+            }
+            catch (CryptographicException e)
+            {
+                throw new Exception("RSA decryption unsuccessful", e);
+            }
+
+            Console.WriteLine("RSA decryption finished");
+            return result;
+        }
+    }
+}
